4cd98d897efbc2cafe70fd18d7297f554594d278
root <root@openshift.example.com>
2015-12-03 20:36:58 +0000

    binaries

--- bin/cleanup-envs.sh
+++ bin/cleanup-envs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PROJECT="_null_"
-PROJECTS="testing development test bluegreen abdeployment mlbparks"
+PROJECTS="`oc get projects -o name | sed -e 's/project\///'`"
 DEMO_USER="demo"
 
 . ../libs/functions
@@ -11,6 +11,16 @@ usage() {
   echo "${0} -p [ project_name | all ]"
   exit
 }
+
+# No point doing anything if we don't have any projects!
+if [ `echo ${PROJECTS} | wc -w` -eq 0 ]
+then
+  echo "No projects to delete here!"
+  exit
+fi
+
+#echo "Existing projects:"
+#echo "${PROJECTS}"
 
 # parse arguments
 while getopts ":p:" opt
@@ -37,23 +47,33 @@ do
   esac
 done
 
-# bail out if we don't have the right arguments
+# ask for project if none passed as args and we have something to delete!
 if [ "${PROJECT}" = "_null_" ]
 then
-  usage
+  echo "Choose project to delete:"
+  select PROJECT in ${PROJECTS}
+  do
+    break
+  done
 fi
 
 # START
 echo
-echo "REMOVE DEMO APPS..."
+echo "REMOVE PROJECT(S)..."
 echo
 
+# Need to login into demo namespace...
 echo "Logging in as ${DEMO_USER} user..."
 oc login -u ${DEMO_USER}
 
-for p in ${PROJECT}
-do
-  echo "Deleting project \"${p}\"..."
-  oc delete project ${p} 2>/dev/null
-done
-
+# Finally, do the dirty work
+if [ "${PROJECT}" != "_null_" -a "${PROJECT}" != "" ]
+then
+  for p in ${PROJECT}
+  do
+    echo "Deleting project \"${p}\"..."
+    oc delete project ${p} 2>/dev/null
+  done
+else
+  exit
+fi
---
+++ bin/setup-efk-logging.sh
@@ -0,0 +1,95 @@
+#!/usr/bin/bash
+
+#
+# Setup OSE centralised container logs using EFK
+#
+
+OSE_MASTER="openshift.example.com"
+LIBDIR="../libs"
+CONFDIR="../conf"
+USER="demo"
+MDSA="metrics-deployer-service-account.yml"
+PROJECT="logging"
+
+. ${LIBDIR}/functions
+
+# START
+echo
+echo "*** SETUP OPENSHIFT EFK LOGGING ***"
+echo "*** THIS USES AUTO GENERATED CERTS AND NON-PERSISTENT METRICS STORAGE ***"
+echo
+
+# Need to run as root so we are SYSTEM:ADMIN
+if [ ${MY_UID} -ne 0 ]
+then
+  echo "Need to be root for this one!"
+  exit 1
+fi
+
+run_cmd echo "Log in to the ${PROJECT} project..."
+run_cmd run "oc project ${PROJECT}"
+
+if [ ! -r ${CONFDIR}/${MDSA} ]
+then
+  echo "Whoops! ${MDSA} is missing :("
+  echo "Creating a sample one ..."
+  mkdir ${CONFDIR} >/dev/null 2>&1
+  cat > ${CONFDIR}/${MDSA} <<EOF
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: metrics-deployer
+secrets:
+- name: metrics-deployer
+EOF
+  chown -R demo:demo ${CONFDIR} >/dev/null 2>&1
+  chmod 755 ${CONFDIR} >/dev/null 2>&1
+  chmod 644 ${CONFDIR}/${MDSA} >/dev/null 2>&1
+fi
+
+run_cmd echo "Creating Metrics Deployer Service Account..."
+run_cmd run "oc create -f ${CONFDIR}/${MDSA}"
+
+run_cmd echo "Grant correct permissions to accounts..."
+run_cmd run "oadm policy add-role-to-user edit system:serviceaccount:openshift-infra:metrics-deployer"
+run_cmd run "oadm policy add-cluster-role-to-user cluster-reader system:serviceaccount:openshift-infra:heapster"
+
+# uncomment the following to use your own certs
+#oc secrets new metrics-deployer hawkular-metrics.pem=/home/openshift/metrics/hm.pem \
+#hawkular-metrics-ca.cert=/home/openshift/metrics/hm-ca.cert
+
+# else you can use the default auto-generated certs...
+run_cmd echo "Use auto-generated certs for trust..."
+run_cmd run "oc secrets new metrics-deployer nothing=/dev/null"
+
+METRICS="metrics.yaml"
+METRICS_TEMPLATE="/usr/share/ansible/openshift-ansible/roles/openshift_examples/files/examples/infrastructure-templates/enterprise/metrics-deployer.yaml"
+
+echo "Ensuring we have a metrics config file..."
+if [ ! -r ${CONFDIR}/{METRICS} ]
+then
+  if [ -r ${METRICS_TEMPLATE} ]
+  then
+    cp ${METRICS_TEMPLATE} ${CONFDIR}/{METRICS}
+  fi
+fi
+
+run_cmd echo "Setting up Hawkular metrics...this will run for some time in the background..."
+run_cmd run "oc process -f ${CONFDIR}/${METRICS} -v IMAGE_PREFIX=openshift3/,IMAGE_VERSION=latest,HAWKULAR_METRICS_HOSTNAME=${OSE_MASTER},USE_PERSISTENT_STORAGE=false | oc create -f -"
+
+OSE_MASTER_CONFIG=/etc/origin/master/master-config.yaml
+TIMESTAMP="`date +%d%m%y_%m%S`"
+
+echo "Updating OSE Master Config (adding Metrics URL...)"
+if [ -r ${OSE_MASTER_CONFIG} ]
+then
+  grep "metricsPublicURL:" ${OSE_MASTER_CONFIG} >/dev/null 2>&1 || \
+  ( cp ${OSE_MASTER_CONFIG} ${OSE_MASTER_CONFIG}.${TIMESTAMP} && \
+    echo "Made a copy of master file - ${OSE_MASTER_CONFIG}.${TIMESTAMP}" && \
+   sed -i '/assetConfig:/a\ \ metricsPublicURL: https://openshift.example.com/hawkular/metrics' ${OSE_MASTER_CONFIG} )
+fi
+
+echo "Restating Openshift Master..."
+systemctl restart atomic-openshift-master 
+
+echo "WE SHOULD BE DONE HERE!"
